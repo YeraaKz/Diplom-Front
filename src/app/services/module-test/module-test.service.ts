744e2d3df6c1a3d74ec81a0b938b1f6b99b298ec
Yernar <107267454+YeraaKz@users.noreply.github.com>
2024-06-05 19:00:32 +0500

    Update module-test.service.ts

--- src/app/services/module-test/module-test.service.ts
+++ src/app/services/module-test/module-test.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {Observable} from "rxjs";
 import {TestResultResponse} from "../test/test-result-response";
-import {environment} from "../../../environment/environment.prod";
+import {environment} from "../../../environments/environment";
 
 @Injectable({
   providedIn: 'root'
